5d55e58d0de45a2090f2ebe99b233d950476467c
Dorn Davy <dorn.davy93@gmail.com>
2014-12-31 09:21:04 +0700

    Crate user file code

--- ads/create_user.php
+++ ads/create_user.php
@@ -5,41 +5,28 @@ include 'include/libraries.php';
 include "include/restrict.php";
 include "include/css.php";
 ?>
-    <div class="container head ">
+<div class="container head ">
 
-        <div style="margin-top: 50px;">
+    <div style="margin-top: 50px;">
         <div class="header">
-        <?php
+            <?php
             include 'include/menu.php'
 
-        ?>
-     </div>
-       <div class="row" style="margin-top: 40px">
-           <form action="create_code_advertise.php"  method="post">
-               <div class="col-md-6">
-                   <hr/>
-                       <label for="">Customer*:</label>
-                   <select name="customer" id="" class="form-control">
-                       <?php lookup('customer','name_en','id') ?>
-                   </select>
-                   <label for="">User*:</label>
-                   <select name="user" id="" class="form-control" id="e2">
+            ?>
+        </div>
+        <div class="row" style="margin-top: 40px">
+            <form action="create_code_advertise.php" method="post">
+                <div class="col-md-6">
+                    <hr/>
 
-                       <?php lookup('users','en_name','id') ?>
-                   </select>
-               </div>
-               <div class="col-md-6">
-                   <hr/>
-                   <label for="">Date*:</label>
-                   <input type="date" class="form-control" name="date" required=""/><br/>
+                </div>
+                <div class="col-md-6">
+                   
+                </div>
+            </form>
+        </div>
 
-                   <input type="submit" class="btn btn-primary" name="create" value="Create"/>
-                   <input type="reset" class="btn btn-default"/>
-               </div>
-           </form>
-       </div>
-
-       </div>
+    </div>
 </div>
 </div>
 <?php
